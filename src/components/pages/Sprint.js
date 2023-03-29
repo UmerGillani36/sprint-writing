6771c5bad5cd183bf83622052a86e58c011eb0e2
UmerGillani36 <syedomershahgillani.com>
2023-03-29 16:16:22 -0700

    Timer UI done

--- src/components/pages/Sprint.js
+++ src/components/pages/Sprint.js
@@ -1,15 +1,15 @@
-import { Grid,TextField } from "@mui/material";
+import { Grid, TextField } from "@mui/material";
 import React, { useContext, useEffect, useState } from "react";
 import { Navigate, useNavigate } from "react-router-dom";
 import Navbar from "../Navbar";
 import { Outlet } from "react-router-dom";
 import { AuthContext } from "./auth";
-import ReactQuill from 'react-quill';
-import 'react-quill/dist/quill.snow.css';
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/firestore';
-import { collection, onSnapshot, doc, deleteDoc } from 'firebase/firestore';
-import { auth, dbFireStore } from '../pages/auth/Firebase';
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import { collection, onSnapshot, doc, deleteDoc } from "firebase/firestore";
+import { auth, dbFireStore } from "../pages/auth/Firebase";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDDQSIPDECt9fwPSWYwvXO_6V4CI-tsLNg",
@@ -18,17 +18,17 @@ const firebaseConfig = {
   projectId: "writo-525e8",
   storageBucket: "writo-525e8.appspot.com",
   messagingSenderId: "38396057848",
-  appId: "1:38396057848:web:c3f76e6fa268d3cf0d830d"
+  appId: "1:38396057848:web:c3f76e6fa268d3cf0d830d",
 };
 const Sprint = () => {
   const navigate = useNavigate();
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   const handleLogout = () => {
     navigate("/login");
   };
-  const [editorState, setEditorState] = useState('');
+  const [editorState, setEditorState] = useState("");
   const [docRef, setDocRef] = useState(null);
-  const [chapterTitle, setChapterTitle] = useState('');
+  const [chapterTitle, setChapterTitle] = useState("");
 
   useEffect(() => {
     if (!firebase.apps.length) {
@@ -37,7 +37,7 @@ const Sprint = () => {
 
     if (user) {
       const db = firebase.firestore();
-      const docRef = db.collection('arhamdoc').doc(user.uid);
+      const docRef = db.collection("arhamdoc").doc(user.uid);
       setDocRef(docRef);
       docRef.get().then((doc) => {
         if (doc.exists) {
@@ -64,50 +64,90 @@ const Sprint = () => {
 
   return (
     <>
-    {user ? 
-      <div className="page">
-        <Navbar />
-        <Grid container justifyContent="center" sx={{backgroundColor: "", padding: "30px 10%", height: "100vh"}}>
-          <Grid item xs={12} md={8}>
-            <div style={{ position: "relative", display: "inline-block" }}>
-            <TextField
-  variant="standard"
-  fullWidth
-  id="title"
-  label={chapterTitle ? "" : "Chapter Title"}
-  name="title"
-  value={chapterTitle}
-  onChange={handleTitleChange}
-  sx={{
-    position: "absolute",
-    top: -25,
-    left: 0,
-    
-    backgroundColor: "#fff",
-    width: `${chapterTitle.length * 15}px`,
-    borderTopLeftRadius: "8px",
-    borderTopRightRadius: "8px",
-    zIndex: 1,
-    fontWeight: "bold"
-  }}
-  
-/>
+      {user ? (
+        <div className="page">
+          <Navbar />
+          <Grid
+            container
+            justifyContent="center"
+            sx={{
+              backgroundColor: "",
+              padding: "30px 10%",
+              height: "100vh",
+              justifyContent: "space-evenly",
+            }}
+          >
+            <div
+              style={{
+                width: "160px",
+                height: "160px",
+                borderRadius: "50%",
+                // border: "6px soli/d linear-gradient(#e66464, #9198e5)",
+                margin: "50px",
+                background: "linear-gradient(#e66464, #9198e5)",
+                padding: 10,
+              }}
+            >
+              <div
+                style={{
+                  width: "140px",
+                  height: "140px",
+                  borderRadius: "50%",
+                  background: "rgba(0,0,0,0.5)",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <span
+                  style={{
+                    color: "white",
+                    fontWeight: "bold",
+                    fontSize: "28px",
+                  }}
+                >
+                  00:59
+                </span>
+              </div>
             </div>
-            <ReactQuill 
-              style={{backgroundColor: "#fff", height: "calc(100% - 60px)"}}
-              value={editorState} 
-              onChange={handleEditorChange}
-            />
+            <Grid item xs={12} md={8}>
+              <div style={{ position: "relative", display: "inline-block" }}>
+                <TextField
+                  variant="standard"
+                  fullWidth
+                  id="title"
+                  label={chapterTitle ? "" : "Chapter Title"}
+                  name="title"
+                  value={chapterTitle}
+                  onChange={handleTitleChange}
+                  sx={{
+                    position: "absolute",
+                    top: -25,
+                    left: 0,
+
+                    backgroundColor: "#fff",
+                    width: `${chapterTitle.length * 15}px`,
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                    zIndex: 1,
+                    fontWeight: "bold",
+                  }}
+                />
+              </div>
+              <ReactQuill
+                style={{ backgroundColor: "#fff", height: "calc(100% - 60px)" }}
+                value={editorState}
+                onChange={handleEditorChange}
+              />
+            </Grid>
           </Grid>
-        </Grid>
-        <Outlet />
-      </div>
-      :
-      <Navigate to ="/" />
-    }
+          <Outlet />
+        </div>
+      ) : (
+        <Navigate to="/" />
+      )}
     </>
   );
 };
-
 
 export default Sprint;
